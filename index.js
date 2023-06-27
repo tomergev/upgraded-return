a89d5599c6ccac139c1a91111d4b2026f17d93ba
Tomer <tomergev21@gmail.com>
2023-06-27 15:12:16 -0700

    GIPHY api fetching properly

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+require('dotenv').config()
+
+const axios = require('axios')
+const params = { 
+  api_key: process.env.GIPHY_API_KEY,
+  limit: 1,   
+}
+
+const fetch = async () => {
+  try {
+    const res = await axios.get('https://api.giphy.com/v1/gifs/trending', { params })
+    console.log(res)
+  } catch (err) {
+    console.log(err)
+  }
+}
+fetch()
