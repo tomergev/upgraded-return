1ea0281e99c85e3c146f9c6e8f11d28f176edd77
Tomer <tomergev21@gmail.com>
2023-07-20 15:31:42 -0700

    Created a simple discord chatbot that's up and running

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 require('dotenv').config()
 
+const { 
+  Client, 
+  Collection,
+  Events, 
+  GatewayIntentBits,
+  SlashCommandBuilder,
+} = require('discord.js')
+
+try {
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+  
+  client.login(process.env.DISCORD_TOKEN_SECRET)
+  client.once(Events.ClientReady, (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`)
+  })
+  
+  const pingCommand = {
+    data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+    async execute(interaction) {
+      await interaction.reply('Pong!')
+    },
+  }
+
+  client.commands = new Collection()
+  client.commands.set(pingCommand.data.name, pingCommand)
+
+  client.on(Events.InteractionCreate, (interaction) => {
+    if (!interaction.isChatInputCommand()) return
+  
+    const command = interaction.client.commands.get(interaction.commandName)
+  
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`)
+      return
+    }
+  
+    command.execute(interaction).catch((err) => console.error(err))
+  })
+
+} catch (err) {
+  console.error(err)
+}
+
 // const axios = require('axios')
 // const params = { 
 //   api_key: process.env.GIPHY_API_KEY,
@@ -16,28 +59,82 @@ require('dotenv').config()
 // }
 // fetch()
 
-const { Configuration, OpenAIApi } = require('openai')
-const configuration = new Configuration({
-    organization: 'org-LdGf0GJuqZWkSR8buj7OKJCz',
-    apiKey: process.env.OPENAI_API_KEY,
-})
-const openai = new OpenAIApi(configuration)
+// const { Configuration, OpenAIApi } = require('openai')
+// const configuration = new Configuration({
+//     organization: 'org-LdGf0GJuqZWkSR8buj7OKJCz',
+//     apiKey: process.env.OPENAI_API_KEY,
+// })
+// const openai = new OpenAIApi(configuration)
 
-const generateText = async () => {
-  try {
-    // const res = await openai.listEngines()
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system', 
-          content: 'Write me a catchy twitter post about javascript array methods with emojis' 
-        }, 
-      ],
-    })
-    console.log(completion)
-  } catch (err) {
-    console.error(err)
-  }
-}
-generateText()
+// const generateText = async () => {
+//   try {
+//     const completion = await openai.createChatCompletion({
+//       model: 'gpt-3.5-turbo',
+//       messages: [{
+//         role: 'system', 
+//         content: 'Write me a catchy twitter post about javascript array methods with emojis' 
+//       }],
+//     })
+//     console.log(completion)
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+// generateText()
+
+
+// // const { TwitterApi } = require('twitter-api-v2')
+// // const twitterClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN)
+// const axios = require('axios')
+// const crypto = require('crypto')
+// const OAuth = require('oauth-1.0a')
+
+// const getOAuth1aAuthorizationHeader = (url, method, data) => {
+//   const oauth = OAuth({
+//     consumer: { 
+//       key: process.env.TWITTER_API_KEY, 
+//       secret: process.env.TWITTER_API_KEY_SECRET,
+//     },
+//     hash_function: (base_string, key) => crypto.createHmac('sha1', key).update(base_string).digest('base64'),
+//     signature_method: 'HMAC-SHA1',
+//   })
+
+//   const token = {
+//     key: process.env.TWITTER_ACCESS_TOKEN,
+//     secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+//   }
+
+//   const requestData = { url, method, data }
+//   const authorization = oauth.authorize(requestData, token)
+
+//   return oauth.toHeader(authorization)
+// }
+
+// const tweet = async () => {
+//   try {
+//     // const res = await twitterClient.v2.tweet('My tweet text with two images!')
+//     // console.log(twitterClient)
+
+
+//     // const url = 'https://api.twitter.com/2/tweets'
+//     // const headers = {
+//     //   'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+//     //   'Content-Type': 'application/json'
+//     // }
+//     // const data = {
+//     //   text: 'First tweet!'
+//     // }
+//     // const response = await axios.post(url, data, { headers })
+//     // console.log(response)
+
+//     const data = { text: 'First Tweet' }
+//     const url = 'https://api.twitter.com/2/statuses/update'
+//     const headers = getOAuth1aAuthorizationHeader(url, 'POST', data)
+
+//     const response = await axios.post(url, data, { headers })
+//     console.log(response)
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+// tweet()
